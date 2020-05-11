321a4d6c41de196baf6fd1727558fa6d7a03bb10
jason <jason_doan94@yahoo.com>
2020-05-11 19:36:55 -0400

    jquery start

---
+++ jQuery/selectorExercise/index.js
@@ -0,0 +1,11 @@
+//select all div and give background color purple
+$("div").css("background", "purple");
+
+//class highlight and give 200 wide
+$(".highlight").css("width", "200px");
+
+//id of third and orange border
+$("#third").css("border", "2px solid orange");
+
+//bonus: select the first div only and change color to pink
+$("div:first-of-type").css("color", "pink");
